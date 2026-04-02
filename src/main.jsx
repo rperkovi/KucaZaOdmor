37e4153cb07a0e6419ed5e59483ca72f872c274c
Roberto Perkovic <roberto.perkovic01@gmail.com>
2026-04-02 18:16:17 +0200

    smanjivanje slike

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import './App.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -11,3 +12,8 @@ createRoot(document.getElementById('root')).render(
     
   </StrictMode>,
 )
+
+
+function App() {
+  return <img src="../assets" className="slika" alt="Opis" />;
+}
