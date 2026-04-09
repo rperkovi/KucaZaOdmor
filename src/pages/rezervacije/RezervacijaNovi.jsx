f8eaf8f205c7cbbc5ea15723e5335c9e2d41c21b
edunovaProgramiranje <84087934+edunovaProgramiranje@users.noreply.github.com>
2026-04-09 20:44:20 +0200

    Odabir raspona datuma

--- src/pages/rezervacije/RezervacijaNovi.jsx
+++ src/pages/rezervacije/RezervacijaNovi.jsx
@@ -4,72 +4,89 @@ import { RouteNames } from "../../constants";
 import RezervacijaService from "../../services/rezervacije/RezervacijaService";
 import { useEffect, useState } from "react";
 import GostService from "../../services/gosti/GostService";
+import DatePicker, { registerLocale } from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import hr from 'date-fns/locale/hr';
 
-export default function RezervacijaNovi(){
+export default function RezervacijaNovi() {
 
     const navigate = useNavigate()
     const [gosti, setGosti] = useState([])
 
-     useEffect(()=>{
-            ucitajGoste()
-        },[])   
-        
-        async function ucitajGoste() {
-            await GostService.get().then((odgovor)=>{
-    
-                 if(!odgovor.success){
-                    alert('Nije implementiran servis')
-                    return
-                }
-    
-                setGosti(odgovor.data)
-            })
-        }
+    const [dateRange, setDateRange] = useState([null, null]);
+    const [startDate, endDate] = dateRange;
 
-    async function dodaj(rezervacija){
+    registerLocale('hr', hr);
+
+
+    useEffect(() => {
+        ucitajGoste()
+    }, [])
+
+    async function ucitajGoste() {
+        await GostService.get().then((odgovor) => {
+
+            if (!odgovor.success) {
+                alert('Nije implementiran servis')
+                return
+            }
+
+            setGosti(odgovor.data)
+        })
+    }
+
+    async function dodaj(rezervacija) {
         //console.table(smjer) // ovo je za kontrolu da li je sve OK
-        await RezervacijaService.dodaj(rezervacija).then(()=>{
+        await RezervacijaService.dodaj(rezervacija).then(() => {
             navigate(RouteNames.REZERVACIJE)
         })
     }
 
 
-    function odradiSubmit(e){ //e je event
+    function odradiSubmit(e) { //e je event
         e.preventDefault() // nemoj odraditi submit
         const podaci = new FormData(e.target)
-        
-        
-        
-        
+
+
+
+
         dodaj({
             gost: parseInt(podaci.get('gost')),
-            cijena:parseFloat(podaci.get('cijena')),
+            cijena: 100, //parseFloat(podaci.get('cijena')), -- Ovdje će se dovući cijena iz cjenika za to razdoblje
             datumRezervacije: new Date().toISOString(),
-            datumPocetka: new Date(podaci.get('datumPocetka')).toISOString(),
-            datumKraja: new Date(podaci.get('datumKraja')).toISOString(),
+            datumPocetka: startDate.toISOString(),
+            datumKraja: endDate.toISOString(),
             platio: podaci.get('platio') === 'on'
         })
     }
 
 
+    function brojDana() {
+        console.log(endDate)
+        if (endDate == null) {
+            return ''
+        }
+        const razlikaUMilisekundama = Math.abs(endDate - startDate);
+        const milisekundiUDanu = 1000 * 60 * 60 * 24;
+        return Math.round(razlikaUMilisekundama / milisekundiUDanu) + ' dana';
+    }
 
 
 
-
-    return(
+    return (
         <>
-        <h3>
-            Unos nove rezervacije
-        </h3>
-         <Container className="mt-4">
-                    <Card className="shadow-sm">
-                        <Card.Body>
-                            <Card.Title className="mb-4">Podaci o rezervaciji</Card.Title>
-                            <Form onSubmit={odradiSubmit}>
-                           
+            <h3>
+                Unos nove rezervacije
+            </h3>
+            <Container className="mt-4">
+                <Card className="shadow-sm">
+                    <Card.Body>
+                        <Card.Title className="mb-4">Podaci o rezervaciji</Card.Title>
+                        <Form onSubmit={odradiSubmit}>
+
                             <Row>
-                                <Col md={6}>
-                                   <Form.Group controlId="gost" className="mb-3">
+                                <Col md={12}>
+                                    <Form.Group controlId="gost" className="mb-3">
                                         <Form.Label className="fw-bold">Gost</Form.Label>
                                         <Form.Select name="gost" required>
                                             <option value="">Odaberite gosta</option>
@@ -81,11 +98,34 @@ export default function RezervacijaNovi(){
                                         </Form.Select>
                                     </Form.Group>
                                 </Col>
-                                
+                                <Col md={12}>
+                                           <p className="fw-bold form-label">
+                                             Razdoblje rezervacije {brojDana()}
+                                            </p>
+                                        <DatePicker
+                                            name="razdoblje"
+                                            id="razdoblje"
+                                            dateFormat="dd.MM.yyyy."
+                                            locale="hr"
+                                            selectsRange={true}
+                                            startDate={startDate}
+                                            endDate={endDate}
+                                            onChange={(update) => {
+                                                setDateRange(update);
+                                            }}
+                                            isClearable={true}
+                                            // Dodavanje Bootstrap klase input polju
+                                            className="form-control odabirDatuma"
+                                            placeholderText="Klikni za odabir..."
+                                        />
+                                        
+
+                                </Col>
+
                             </Row>
 
-                            <Row className="align-items-center">
-                                
+                            <Row className="align-items-center" style={{marginBottom: '10px'}}>
+
 
                                 {/* Aktivan - Switch umjesto checkboxa za moderniji izgled */}
                                 <Col md={6}>
@@ -112,12 +152,12 @@ export default function RezervacijaNovi(){
                                 </Button>
                             </div>
 
-                            </Form>
-                        </Card.Body>
-                    </Card>
-                </Container>
+                        </Form>
+                    </Card.Body>
+                </Card>
+            </Container>
 
-            
+
         </>
     )
 }
