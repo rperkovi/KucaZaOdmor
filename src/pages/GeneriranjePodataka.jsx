79156b8adfa424e1b852f4ee48db048c886467f7
Roberto Perkovic <roberto.perkovic01@gmail.com>
2026-04-28 19:34:48 +0200

    gen.podataka

--- src/pages/GeneriranjePodataka.jsx
+++ src/pages/GeneriranjePodataka.jsx
@@ -8,3 +8,21 @@ export default function GeneriranejPodataka(){
     </>
     )
 }
+
+
+export const dohvatiRezervacijeSPlatiteljem = (rezervacije, gosti) => {
+    return rezervacije.map(rezervacija => {
+        // Pronađi gosta čija šifra odgovara onoj u rezervaciji
+        const gostPodaci = gosti.find(gost => gost.sifra === rezervacija.gost);
+        
+        return {
+            ...rezervacija,
+            gostDetalji: gostPodaci ? `${gostPodaci.ime} ${gostPodaci.prezime}` : 'Nepoznat gost',
+            emailGosta: gostPodaci?.email || 'Nema emaila'
+        };
+    });
+};
+
+// Primjena u komponenti:
+const prošireneRezervacije = dohvatiRezervacijeSPlatiteljem(rezervacije, gosti);
+console.log(prošireneRezervacije);
