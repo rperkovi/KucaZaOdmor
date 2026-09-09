6d9b519a4fc06ba79c43c99b3962dcf98ba61b4a
Roberto Perkovic <roberto.perkovic01@gmail.com>
2026-09-09 20:32:09 +0200

    izbacio moje unose za viježbu

--- src/services/gosti/GostPodaci.js
+++ src/services/gosti/GostPodaci.js
@@ -1,51 +1,5 @@
 export const gosti = [
     {
-        sifra: 1,
-        ime: 'Luka',
-        prezime: 'Modrić',
-        email: 'lmodric@gmail.com',
-        telefon: '+385 91 123 4567',
-        drzava: 'HR',
-        aktivan: false
-    },
-    {
-        sifra: 2,
-        ime: 'Diego',
-        prezime: 'Maradona',
-        email: 'dmaradona@gmail.com',
-        telefon: '+385 91 234 5678',
-        drzava: 'HR',
-        aktivan: false
-    },
-    {
-        sifra: 3,
-        ime: 'Siniša',
-        prezime: 'Rimac',
-        email: 'srimac@gmail.com',
-        telefon: '+385 91 345 6789',
-        drzava: 'HR',
-        aktivan: false
-    },
-    {
-        sifra: 4,
-        ime: 'Darko',
-        prezime: 'Zubak',
-        email: 'dzubak@hotmail.com',
-        telefon: '+385 91 456 7890',
-        drzava: 'HR',
-        aktivan: false
-
-    },
-    {
-        sifra: 5,
-        ime: 'Zvonko',
-        prezime: 'Zvonkić',
-        email: 'z.zvonkic@gmail.com',
-        telefon: '+385 91 567 8901',
-        drzava: 'HR',
-        aktivan: false
-    },
-    {
         sifra: 6,
         ime: 'Denis',
         prezime: 'Delor',
--- src/services/rezervacije/RezervacijaPodaci.js
+++ src/services/rezervacije/RezervacijaPodaci.js
@@ -1,50 +1,5 @@
 export const rezervacije = [
     {
-        sifra: 1,
-        gost: 1,
-        cijena:240,
-        datumRezervacije: '2026-03-02T17:00:00',
-        datumPocetka: '2026-04-02T17:00:00',
-        datumKraja: '2026-04-04T17:00:00',
-        platio: false
-    },
-    {
-        sifra: 2,
-        gost: 2,
-        cijena:480,
-        datumRezervacije: '2025-11-02T17:00:00',
-        datumPocetka: '2026-05-02T17:00:00',
-        datumKraja: '2026-05-06T17:00:00',
-        platio: false
-    },
-    {
-        sifra: 3,
-        gost: 1,
-        cijena:480,
-        datumRezervacije: '2026-03-02T17:00:00',
-        datumPocetka: '2026-04-28T17:00:00',
-        datumKraja: '2026-05-02T17:00:00',
-        platio: false
-    },
-    {
-        sifra: 4,
-        gost: 3,
-        cijena:2830,
-        datumRezervacije: '2026-05-22T17:00:00',
-        datumPocetka: '2026-08-22T17:00:00',
-        datumKraja: '2026-09-04T17:00:00',
-        platio: false
-    },
-    {
-        sifra: 5,
-        gost: 3,
-        cijena:4200,
-        datumRezervacije: '2026-01-02T17:00:00',
-        datumPocetka: '2026-08-02T17:00:00',
-        datumKraja: '2026-08-14T17:00:00',
-        platio: false
-    },
-    {
         sifra: 6,
         gost: 7,
         cijena: 1260,
